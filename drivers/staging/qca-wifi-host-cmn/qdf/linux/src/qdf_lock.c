41507ee8d2e0c690be518488b6cd114258c785b6
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-28 23:28:33 +0000

    Merge tag 'LA.UM.9.15.2.r1-08300-KAMORTA.QSSI14.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn into rebase
    "LA.UM.9.15.2.r1-08300-KAMORTA.QSSI14.0"
    
    * tag 'LA.UM.9.15.2.r1-08300-KAMORTA.QSSI14.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn: (525 commits)
      qcacmn: Fetch rssi from radar report pulse
      qcacmn: Add support for the 4th dword in radar report pulse
      qcacmn: Increase status size for HTT WBM completion v3
      qcacmn: Check cookie and avoid to read out of bound
      qcacmn: Add all membership selector elements
      qcacmn: Add bss membership selector defines to wlan_cmn_ieee80211.h
      qcacmn: Check function pointer to avoid NULL point dereference
      qcacmn: Possible OOB read in process_fw_diag_event_data
      qcacmn: Fix OOB read issue in SSID ie
      qcacmn: Possible Integer overflow in wifi_pos_oem_rsp_handler
      qcacmn: Fix possible OOB in wmi_extract_dbr_buf_release_entry
      qcacmn: Replace WMI_LOGI() with wmi_* appropriate log level
      qcacmn: Add CDP intf to flush fragments for a particular peer
      qcacmn: Add length check in beacon IE parsing function
      qcacmn: Avoid checking extcaps byte if equal to IE len
      qcacmn: handle IPA buffer smmu map/unmap correctly
      qcacmn: Optimize congestion calculation logic
      qcacmn: Increment skb users for TSO pkt before enqueue of first segment
      qcacmn: Fix out of bound read issue in ESP ie parse
      qcacmn: Add wlan status code enum
      ...
    
    Change-Id: I888e9024fe52c10f5011c5bf8d9ffb4d28df8b70

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
@@ -23,7 +23,7 @@
 
 #include <qdf_types.h>
 #include <i_host_diag_core_event.h>
-#ifdef FEATURE_RUNTIME_PM
+#ifdef CONFIG_MCL
 #include <cds_api.h>
 #include <hif.h>
 #endif
@@ -493,6 +493,10 @@ qdf_export_symbol(__qdf_runtime_lock_init);
 void qdf_runtime_lock_deinit(qdf_runtime_lock_t *lock)
 {
 	void *hif_ctx = cds_get_context(QDF_MODULE_ID_HIF);
+
+	if (!lock)
+		return;
+
 	hif_runtime_lock_deinit(hif_ctx, lock->lock);
 }
 qdf_export_symbol(qdf_runtime_lock_deinit);
@@ -734,7 +738,6 @@ void qdf_lock_stats_deinit(void)
 				  __func__, lock_cookies[i].u.cookie.func,
 				  lock_cookies[i].u.cookie.line);
 	}
-	lock_cookie_freelist = NULL;
 }
 
 /* allocated separate memory in case the lock memory is freed without
